557480c952f0553f3051ba2d0107afdd5c673d0a
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-01-25 18:24:36 +0000

    Create Receiver.elm

---
+++ src/Receiver.elm
@@ -0,0 +1,9 @@
+module Receiver exposing (..)
+
+{-  Here we will have:
+    1. Hight set of cross dipoles (good at seeing way out)
+    2. Low set of cross dipoles (better at close range)
+    3. Goniometer to blend signals from cross dipoles
+    4. Gonio in height mode to blend low and high signals
+-}
+
