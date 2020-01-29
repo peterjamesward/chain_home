0726f7fff61641d6f0471bab841742baf5ae7d65
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-01-29 15:45:29 +0000

    Non-movable pointer

--- src/Goniometer.elm
+++ src/Goniometer.elm
@@ -18,6 +18,25 @@ showGonioImage m =
     --      , H.style "transform" ("rotate(" ++ (String.fromInt <| truncate <| m.goniometer * 180/pi) ++ "deg)") 
     --      ] []
 
+drawGoniometerPointer theta = 
+    let originX = 0
+        originY = 0
+        radius = 150
+        xPoint = String.fromFloat <| ((+) originX) 
+                                  <| ((*) radius) <| sin theta
+        yPoint = String.fromFloat <| ((-) originY)
+                                  <| ((*) radius) <| cos theta
+    in
+        Svg.line
+            [ x1 "0"
+            , y1 "0"
+            , x2 xPoint
+            , y2 yPoint
+            , stroke "black"
+            , strokeWidth "10"
+            , strokeLinecap "round"
+            ]
+            []
 
 drawGoniometerScale = 
     let originX = 0
@@ -25,14 +44,14 @@ drawGoniometerScale =
         radius = 160
         xFromIndex i = String.fromFloat <| ((+) originX) 
                                         <| ((*) radius) <| sin <| degrees 
-                                        <| toFloat <| i * 10
+                                        <| toFloat <| i * 10 
         yFromIndex i = String.fromFloat <| ((-) originY)
                                         <| ((*) radius) <| cos <| degrees 
-                                        <| toFloat <| i * 10
+                                        <| toFloat <| i * 10 
         labelPoint i = Svg.text_  [  x <| xFromIndex i
                                    , y <| yFromIndex i
                                    , fill "black"
-                                   , textAnchor "center" 
+                                   , textAnchor "middle" 
                                    ] 
                                    [ Svg.text (String.fromInt (i*10)) 
                                    ]
@@ -41,4 +60,4 @@ drawGoniometerScale =
         , S.width "300"
         , S.height "300"
         ]
-        (List.map labelPoint (List.range 0 35))
+        ((drawGoniometerPointer 0.0) :: (List.map labelPoint (List.range 0 35)))
