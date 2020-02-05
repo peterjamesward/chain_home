50b275038de91d8eb6f0e6bc0b53467b1ad72489
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-02-05 13:19:20 +0000

    Keyboard control of goniometer restored.

--- src/Goniometer.elm
+++ src/Goniometer.elm
@@ -1,81 +1,126 @@
-module Goniometer exposing (showGonio, clickableGonioImage)
+module Goniometer exposing (clickableGonioImage, showGonio)
 
 import Html exposing (..)
 import Html.Attributes as H exposing (..)
 import Svg exposing (..)
 import Svg.Attributes as S exposing (..)
 
+
+
 -- DEBUG
-showGonio m = Html.text <| String.fromInt <| truncate <| m.goniometer * 180.0 / pi
+
+
+showGonio m =
+    Html.text <| String.fromInt <| truncate <| m.goniometer * 180.0 / pi
+
+
 
 -- Assumes a suitably styled DIV is in effect ...
-clickableGonioImage  theta = 
-    [ svg [ viewBox "-200 -200 400 400"
-          , S.width "300"
-          , S.height "300"
-          ]
-       (drawGoniometerScale ++ (drawGoniometerPointer theta))
+
+
+clickableGonioImage theta =
+    svg
+        [ viewBox "-200 -200 400 400"
+        , S.width "300"
+        , S.height "300"
+        ]
+        (drawGoniometerScale ++ drawGoniometerPointer theta)
+
+
+drawGoniometerPointer theta =
+    let
+        originX =
+            0
+
+        originY =
+            0
+
+        radius =
+            135
+
+        xPoint =
+            String.fromFloat <|
+                (+) originX <|
+                    (*) radius <|
+                        sin theta
+
+        yPoint =
+            String.fromFloat <|
+                (-) originY <|
+                    (*) radius <|
+                        cos theta
+    in
+    [ Svg.circle
+        [ cx "0"
+        , cy "0"
+        , r "140"
+        , stroke "grey"
+        , strokeWidth "1"
+        , fill "darkslategrey"
+        ]
+        []
+    , Svg.line
+        [ x1 "0"
+        , y1 "0"
+        , x2 xPoint
+        , y2 yPoint
+        , stroke "antiquewhite"
+        , strokeWidth "10"
+        , strokeLinecap "round"
+        ]
+        []
     ]
 
 
-drawGoniometerPointer theta = 
-    let originX = 0
-        originY = 0
-        radius = 135
-        xPoint = String.fromFloat <| ((+) originX) 
-                                  <| ((*) radius) <| sin theta
-        yPoint = String.fromFloat <| ((-) originY)
-                                  <| ((*) radius) <| cos theta
+drawGoniometerScale =
+    let
+        originX =
+            0
+
+        originY =
+            0
+
+        radius =
+            160
+
+        xFromIndex i =
+            String.fromFloat <|
+                (+) originX <|
+                    (*) radius <|
+                        sin <|
+                            degrees <|
+                                toFloat <|
+                                    i
+                                        * 10
+
+        yFromIndex i =
+            String.fromFloat <|
+                (-) (originY + 4) <|
+                    (*) radius <|
+                        cos <|
+                            degrees <|
+                                toFloat <|
+                                    i
+                                        * 10
+
+        labelPoint i =
+            Svg.text_
+                [ x <| xFromIndex i
+                , y <| yFromIndex i
+                , fill "antiquewhite"
+                , textAnchor "middle"
+                , fontFamily "monospace"
+                ]
+                [ Svg.text (String.fromInt (i * 10))
+                ]
     in
-        [    Svg.circle
-                [ cx "0"
-                , cy "0"
-                , r "140"
-                , stroke "grey"
-                , strokeWidth "1"
-                , fill "darkslategrey"
-                ]
-                []
-           , Svg.line
-                [ x1 "0"
-                , y1 "0"
-                , x2 xPoint
-                , y2 yPoint
-                , stroke "antiquewhite"
-                , strokeWidth "10"
-                , strokeLinecap "round"
-                ]
-                []
+    Svg.circle
+        [ cx "0"
+        , cy "0"
+        , r "180"
+        , stroke "grey"
+        , strokeWidth "1"
+        , fill "dimgrey"
         ]
-
-drawGoniometerScale  = 
-    let originX = 0
-        originY = 0
-        radius = 160
-        xFromIndex i = String.fromFloat <| ((+) originX) 
-                                        <| ((*) radius) <| sin <| degrees 
-                                        <| toFloat <| i * 10 
-        yFromIndex i = String.fromFloat <| ((-) (originY + 4)) 
-                                        <| ((*) radius) <| cos <| degrees 
-                                        <| toFloat <| i * 10 
-        labelPoint i = Svg.text_  [  x <| xFromIndex i
-                                   , y <| yFromIndex i
-                                   , fill "antiquewhite"
-                                   , textAnchor "middle" 
-                                   , fontFamily "monospace"
-                                   ] 
-                                   [ Svg.text (String.fromInt (i*10)) 
-                                   ]
-    in
-        (Svg.circle
-                [ cx "0"
-                , cy "0"
-                , r "180"
-                , stroke "grey"
-                , strokeWidth "1"
-                , fill "dimgrey"
-                ]
-                []
-        ) ::
-        (List.map labelPoint (List.range 0 35))
-
+        []
+        :: List.map labelPoint (List.range 0 35)
--- src/Main.elm
+++ src/Main.elm
@@ -4,32 +4,60 @@ module Main exposing (main)
    This Main module now based on the elm-ui example by Alex Korban
    in his book "Practical Elm"".
 -}
--- Project imports
 
+import BeamSmoother exposing (beamPath)
 import Browser
+import Browser.Events exposing (..)
 import Config exposing (..)
-import Element exposing (..)
+import Constants exposing (..)
+import Echo exposing (..)
+import Element as E exposing (..)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
 import Element.Input as Input
-import Html
+import Goniometer exposing (clickableGonioImage, showGonio)
+import Json.Decode as D exposing (..)
+import Receiver exposing (goniometerMix)
+import Skyline exposing (EdgeSegment, deriveSkyline, viewEdge, viewLineSegment)
+import Station exposing (..)
+import Svg as S exposing (..)
+import Svg.Attributes as SA exposing (..)
+import Target exposing (..)
+import Time
+import Utils exposing (..)
 
 
 type Page
     = InputPage
+    | OperatorPage
 
 
 type Msg
     = NoOp
-    | ChangePlanText String
-    | SubmitPlan
     | SetConfigStateMsg Int Bool
+    | DisplayReceiver
+    | DisplayConfiguration
+    | Tick Time.Posix
+    | KeyChanged Bool String
 
 
 type alias Model =
     { currPage : Page
-    , currPlanText : String
+    , zone : Time.Zone
+    , startTime : Int -- millseconds from t-zero
+    , time : Int
+    , lineData : List ( Float, Float )
+    , station : Station
+    , targets : List Target
+    , movedTargets : List Target
+    , polarTargets : List PolarTarget
+    , echoes : List Echo
+    , skyline : List ( ( Float, Float ), ( Float, Float ) )
+    , goniometer : Float
+    , gonioOutput : List Echo
+    , keys : Keys
+    , gonioDrag : Maybe ( Float, ( Float, Float ) ) -- angle and mouse position when mouse down
     , activeConfigurations : List TargetSelector
     }
 
@@ -41,11 +69,68 @@ type alias Flags =
 init : Flags -> ( Model, Cmd Msg )
 init _ =
     ( { currPage = InputPage
-      , currPlanText = "hello world"
+      , zone = Time.utc
+      , startTime = 0
+      , time = 0
+      , lineData = beamPath []
+      , station = bawdsey
+      , targets = getAllTargets targetConfigurations
+      , movedTargets = []
+      , polarTargets = []
+      , echoes = []
+      , skyline = []
+      , goniometer = degrees 10 -- relative to Line Of Shoot.
+      , gonioOutput = []
+      , keys = noKeys
+      , gonioDrag = Nothing
       , activeConfigurations = targetConfigurations
       }
     , Cmd.none
     )
+
+
+type alias Keys =
+    { gonioClock : Bool -- A
+    , gonioAnti : Bool -- Q
+    }
+
+
+noKeys : Keys
+noKeys =
+    Keys False False
+
+
+updateKeys : Bool -> String -> Keys -> Keys
+updateKeys isDown key keys =
+    case key of
+        "q" ->
+            { keys | gonioAnti = isDown }
+
+        "a" ->
+            { keys | gonioClock = isDown }
+
+        _ ->
+            keys
+
+
+
+-- When we have mouse tracking we can have non-integer movements.
+
+
+swingGoniometer : Float -> Keys -> Float
+swingGoniometer angle keys =
+    case ( keys.gonioClock, keys.gonioAnti ) of
+        ( True, True ) ->
+            angle
+
+        ( True, False ) ->
+            angle + degrees 1.0
+
+        ( False, True ) ->
+            angle - degrees 1.0
+
+        _ ->
+            angle
 
 
 
@@ -54,11 +139,82 @@ init _ =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Sub.batch
+        [ Time.every 50 Tick
+        , onKeyUp (D.map (KeyChanged False) (D.field "key" D.string))
+        , onKeyDown (D.map (KeyChanged True) (D.field "key" D.string))
+        ]
+
+
+type alias LineData =
+    List ( Float, Float )
+
+
+rangeScale =
+    List.map
+        (\i ->
+            S.text_
+                [ x (String.fromInt (i * 50))
+                , y "-10"
+                , SA.fill "lightgreen"
+                , textAnchor "right"
+                , fontFamily "monospace"
+                ]
+                [ S.text (String.fromInt (i * 5)) ]
+        )
+        (List.range 0 19)
+
+
+scalePathToDisplay : LineData -> LineData
+scalePathToDisplay unscaled =
+    let
+        scalePoint ( x, y ) =
+            ( viewWidth * x / scaleWidthKilometers / 1000, y * strengthToHeightFactor )
+
+        -- TODO: constants!
+    in
+    List.map scalePoint unscaled
 
 
 
 -- UPDATE
+
+
+deriveModelAtTime : Model -> Int -> Model
+deriveModelAtTime model t =
+    let
+        targetsNow =
+            List.map (targetAtTime t model.startTime) model.targets
+
+        convertedTargets =
+            List.map (mapToPolar bawdsey) targetsNow
+
+        echoSignals =
+            deriveEchoes convertedTargets t
+
+        gonioOut =
+            goniometerMix model.goniometer echoSignals
+
+        newSkyline =
+            deriveSkyline (scaleWidthKilometers * 1000) gonioOut
+    in
+    { model
+        | startTime =
+            if model.startTime == 0 then
+                t
+
+            else
+                model.startTime
+        , time = t
+        , targets = getAllTargets model.activeConfigurations
+        , movedTargets = targetsNow
+        , polarTargets = convertedTargets
+        , echoes = echoSignals
+        , skyline = newSkyline
+        , gonioOutput = gonioOut
+        , lineData = scalePathToDisplay <| beamPath newSkyline
+        , goniometer = swingGoniometer model.goniometer model.keys
+    }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -72,6 +228,26 @@ update msg model =
             , Cmd.none
             )
 
+        DisplayReceiver ->
+            ( { model | currPage = OperatorPage }
+            , Cmd.none
+            )
+
+        DisplayConfiguration ->
+            ( { model | currPage = InputPage }
+            , Cmd.none
+            )
+
+        Tick newTime ->
+            ( deriveModelAtTime model (Time.posixToMillis newTime)
+            , Cmd.none
+            )
+
+        KeyChanged isDown key ->
+            ( { model | keys = updateKeys isDown key model.keys }
+            , Cmd.none
+            )
+
         _ ->
             ( model, Cmd.none )
 
@@ -80,14 +256,77 @@ update msg model =
 -- VIEW
 
 
+operatorPage : Model -> Element Msg
+operatorPage model =
+    row
+        [ E.spacing 10
+        , E.padding 10
+        , E.centerX
+        ]
+    <|
+        [ E.html <| clickableGonioImage <| model.goniometer + model.station.lineOfShoot
+        , E.html (crt model)
+        ]
+
+
+crt m =
+    let
+        svgPointList =
+            polyLineFromCoords m.lineData
+    in
+    svg
+        [ viewBox "-10 -40 1020 450"
+        , SA.width "1020"
+        , SA.height "420"
+        ]
+    <|
+        [ rect
+            [ x "-10"
+            , y "-40"
+            , SA.width "1020"
+            , SA.height "450"
+            , SA.fill "black"
+            , stroke "black"
+            , strokeWidth "3"
+            , strokeLinejoin "round"
+            ]
+            []
+        , polyline
+            [ points svgPointList
+            , SA.fill "none"
+            , stroke "forestgreen"
+            , opacity "60%"
+            , strokeWidth "2.5"
+            ]
+            []
+        , polyline
+            [ points svgPointList
+            , SA.fill "none"
+            , stroke "springgreen"
+            , strokeWidth "0.8"
+            ]
+            []
+        ]
+            ++ rangeScale
+
+
 view : Model -> Browser.Document Msg
 view model =
+    let
+        content =
+            case model.currPage of
+                OperatorPage ->
+                    operatorPage model
+
+                InputPage ->
+                    inputPage model
+    in
     { title = "VisExp"
     , body =
         [ layout [] <|
-            column [ width fill, spacingXY 0 20 ]
+            column [ E.width E.fill, spacingXY 0 20 ]
                 [ navBar
-                , inputPage model
+                , content
                 ]
         ]
     }
@@ -121,18 +360,19 @@ white =
 navBar : Element Msg
 navBar =
     row
-        [ width fill
+        [ E.width E.fill
         , paddingXY 60 10
         , Border.widthEach { bottom = 1, top = 0, left = 0, right = 0 }
         , Border.color blue
         ]
-        [ el [ alignLeft ] <| text "Chain Home"
-        , el [ alignRight ] <| text "Menu"
+        [ el [ alignLeft ] <| E.text "Chain Home"
+        , el [ alignRight ] <| E.text "Menu"
         ]
 
 
 
 -- Show list of configurations with Checkboxes.
+-- This will now be on its own page with elm-ui.
 
 
 setConfig : TargetSelector -> Bool -> Msg
@@ -146,17 +386,17 @@ targetSelector active =
         display : TargetSelector -> Element Msg
         display g =
             Input.checkbox
-                [ height (px 40)
+                [ E.height (px 40)
                 , Border.width 1
                 , Border.rounded 5
                 , Border.color lightCharcoal
-                , padding 3
+                , padding 10
                 ]
                 { onChange = setConfig g
                 , checked = g.active
                 , label =
                     Input.labelRight [] <|
-                        text g.description
+                        E.text g.description
                 , icon = Input.defaultCheckbox
                 }
     in
@@ -166,7 +406,7 @@ targetSelector active =
 inputPage : Model -> Element Msg
 inputPage model =
     column
-        [ width (px 800)
+        [ E.width (px 800)
         , spacingXY 0 10
         , centerX
         ]
@@ -181,11 +421,11 @@ inputPage model =
                     , Font.color white
                     , paddingXY 20 6
                     , alignRight
-                    , width (px 200)
-                    , height (px 40)
+                    , E.width (px 200)
+                    , E.height (px 40)
                     ]
-                    { onPress = Just SubmitPlan
-                    , label = el [ centerX ] <| text "Go!"
+                    { onPress = Just DisplayReceiver
+                    , label = el [ centerX ] <| E.text "Go!"
                     }
                ]
 
