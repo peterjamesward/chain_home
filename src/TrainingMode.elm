ffdb22e99e729533407d5432eedf5173ce510212
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-03-13 23:12:13 +0000

    CHanged stacking to restore movement to knobs.

--- src/TrainingMode.elm
+++ src/TrainingMode.elm
@@ -1,4 +1,4 @@
-module TrainingMode exposing (advanceTutorial, tutorialModeNSEW, welcomePrompt, tutorialTextBox)
+module TrainingMode exposing (advanceTutorial, tutorialModeNSEW, tutorialTextBox, welcomePrompt)
 
 {-
    This aims to be a "wrapper" of sorts for the Operator page,
@@ -85,62 +85,76 @@ tutorialTextBox model =
             Nothing ->
                 none
 
-            Just prompt ->
-                el [ padding 10
-                , centerY
-                , centerX
-                , width (px 500) ] <|
-                    paragraph
-                        [ Background.color blue
-                        , Border.color white
-                        , Border.rounded 5
-                        , Font.color white
-                        , spacing 4
-                        , padding 4
-                        , pointer
-                        , onClick TutorialAdvance
-                        ]
-                        [ text <|
-                            case prompt of
-                                TutorialWelcome ->
-                                    """Click on these text boxes to learn about the Chain Home receiver and the operator's work.
+            Just stage ->
+                el
+                    [ width fill
+                    , centerY
+                    , centerX
+                    , width (px 500)
+                    , moveUp 100
+                    , moveLeft 80
+                    , inFront <|
+                        el
+                            []
+                        <|
+                            paragraph
+                                [ Background.color blue
+                                , Border.color white
+                                , Border.width 1
+                                , Border.rounded 5
+                                , Font.color white
+                                , spacing 4
+                                , padding 4
+                                , pointer
+                                , onClick TutorialAdvance
+                                ]
+                                (tutorialText stage)
+                    ]
+                    (text "")
+
+
+tutorialText stage =
+    [ text <|
+        case stage of
+            TutorialWelcome ->
+                """Click on these text boxes to learn about the Chain Home receiver and the operator's work.
                             """
 
-                                TutorialDescribeCRT ->
-                                    """The main feature is the Cathode Ray Tube (CRT) that displays signals
+            TutorialDescribeCRT ->
+                """The main feature is the Cathode Ray Tube (CRT) that displays signals
                             returned from radio wave pulses sent out from the transmitter.
                             """
 
-                                TutorialRangeScale ->
-                                    """The numbers along the top show the range in miles.
+            TutorialRangeScale ->
+                """The numbers along the top show the range in miles.
                             Generally, incoming aircraft will appear towards the right hand side.
                             The operator will move the pointer at the top, using the right hand knob below, to
                             identify a particular echo for study.
                             """
 
-                                TutorialCRTTrace ->
-                                    """The line on the CRT moves rapidly from left to right and "dips" for any
+            TutorialCRTTrace ->
+                """The line on the CRT moves rapidly from left to right and "dips" for any
                             returned signals detected. The continuous movement is just noise in the system.
                             The large dips between 0 and 10 miles are from fixed objects near the station.
                             """
 
-                                TutorialIncomingRaid ->
-                                    """In fact, there is an incoming raid now. It's highlighted in white but would
+            TutorialIncomingRaid ->
+                """In fact, there is an incoming raid now. It's highlighted in white but would
                             normally be green. We have to learn as much information about the raid as we can.
                             """
 
-                                TutorialAdjustRange ->
-                                    """Turn the right hand knob (you can click on it or touch it and drag it around)
+            TutorialAdjustRange ->
+                """Turn the right hand knob (you can click on it or touch it and drag it around)
                             and see that the range indicator moves. Try to position the range indicator at the
                             left edge of the "dip" that marks the raid. This will tell you the range of the raid.
                             """
 
-                                TutorialFindBearing ->
-                                    """Now turn the left hand knob (the goniometer) until the "dip" disappears, or
+            TutorialFindBearing ->
+                """Now turn the left hand knob (the goniometer) until the "dip" disappears, or
                             as small as you can make it. The goniometer now indicates the bearing of the incoming
                             raid. Be careful though,
                             """
 
-                                _ ->
-                                    "Somebody needs to write my explanation."
-                        ]
+            _ ->
+                "Somebody needs to write my explanation."
+    ]
