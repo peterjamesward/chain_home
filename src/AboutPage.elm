829de994f8964372ace3ee8b1e220c8d08e853a9
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-03-24 18:04:32 +0000

    ABout text better in Paragraph block.

--- src/AboutPage.elm
+++ src/AboutPage.elm
@@ -5,27 +5,29 @@ import Element exposing (..)
 import Element.Font as Font
 import Html exposing (Html)
 import Html.Attributes exposing (class)
-import Markdown
+import Markdown exposing (defaultOptions)
 import Messages exposing (Msg)
 
 
 aboutPage : Element Msg
 aboutPage =
-    el [ Font.color white, padding 20 ] <|
-        html content
+    paragraph [ Font.color white, padding 20, width fill ]
+        [ html content ]
 
 
 content : Html msg
 content =
-    Markdown.toHtml [ class "content" ] """
+    Markdown.toHtmlWith { defaultOptions | smartypants = True }
+        [ class "content" ]
+        """
 
 ## What is this?
 
 This application is meant to give an impression of what it would have been like to be an operator
 at a Chain Home station during the Battle of Britain in 1940.
 
-Radar technology had been developed rapidly to prepare for the war, and was constantly being updated.
-By modern standards, it was primitive. Rather than the circular screen familiar today, with aircraft
+Scientists and engineers developed radio technology rapidly to prepare for the war, and were constantly
+improving it. By modern standards, it was primitive. Rather than the circular screen familiar today, with aircraft
 conveniently labelled, the operator would spend hours studying the "tube" looking for faint
 signals amongst the noise.
 
@@ -38,13 +40,13 @@ We have picked five distinctive patterns of incoming aircraft. You can learn abo
 would appear on the tube, and what the operator would do to try to find the bearing and the height
 so that this information can be passed on to Fighter Command.
 
-You can then try to keep up with a series on incoming raids. The operator would keep track of
+You can then try to keep up with a series of incoming raids. The operator had to keep track of all
 the raids and send frequent updates on their position to Fighter Command. Keeping
 track of all the raids is not easy.
 
 ## Credits
 
-Application code written by Peter Ward. Historical input by Sharon Ward.
+Application code written by Peter Ward. Historical research by Sharon Ward.
 The application is written in Elm (elm-lang.org) and uses elm-ui and WebGL for the display.
 
 """
