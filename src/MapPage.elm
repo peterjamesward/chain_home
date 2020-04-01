b48f7693d3c92f5792faff88840714c1feda9ab1
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-04-01 14:31:00 +0100

    Consolidated plot drawing. Not shorter but less repetetetive. Need to add the correlated plots.

--- src/MapPage.elm
+++ src/MapPage.elm
@@ -18,6 +18,7 @@ import Messages exposing (Msg(..))
 import Model exposing (Model)
 import Svg exposing (..)
 import Svg.Attributes as A exposing (..)
+import Types exposing (PlotType(..), RecordedPlot)
 import Utils exposing (edges, motorwaySign)
 
 
@@ -35,7 +36,7 @@ mapScale =
     toFloat squareSize / 100000.0
 
 
-stationPos model =
+stationPos =
     -- Pin our station on the map. This needs to be a station attribute because the calculator needs it.
     ( (1.0 + bawdsey.gridSquareEasting) * squareSize
     , (3.0 - bawdsey.gridSquareNorthing) * squareSize
@@ -143,12 +144,17 @@ theMap model =
                     ]
                     []
                 ]
-                    ++ raidTracks model
-                    ++ userPlots model
+                    ++ (if model.actualTraceVisibleOnMap then
+                            plotPlotter <| List.concatMap .positionHistory model.targets
+
+                        else
+                            []
+                       )
+                    ++ plotPlotter model.storedPlots
                     ++ ourStation model
 
 
-raidTracks model =
+plotPlotter plots =
     -- Note that the plots are associated with the polar targets.
     -- We shall use the same conversion from range and bearing to map coordinates as we
     -- do for the calculator grid so this should be fair.
@@ -156,62 +162,59 @@ raidTracks model =
     -- Scalewise, our map is made of 100km squares and they occupy 'squareSize' on the screen.
     let
         ( stationX, stationY ) =
-            stationPos model
+            stationPos
 
         noMoreThan100Miles plot =
             plot.range <= 160000
 
-        raidTrack target =
-            List.map plotArrow <|
-                List.filter noMoreThan100Miles
-                    target.positionHistory
+        timedPlotStyles =
+            [ r "3"
+            , stroke "orange"
+            , strokeWidth "1"
+            , A.fill "navy"
+            ]
+
+        userPlotStyles =
+            [ r "5"
+            , stroke "navy"
+            , strokeWidth "1"
+            , A.fill "yellow"
+            ]
+
+        correlatedPlotStyles =
+            [ r "5"
+            , stroke "orange"
+            , strokeWidth "1"
+            , A.fill "navy"
+            ]
+
+        plotStyles plotType =
+            case plotType of
+                UserPlot ->
+                    userPlotStyles
+
+                TimedPlot ->
+                    timedPlotStyles
+
+                ActualPlot ->
+                    correlatedPlotStyles
 
         plotArrow plot =
             Svg.circle
-                [ cx <| String.fromFloat <| stationX + plot.range  * sin plot.bearing * mapScale
-                , cy <| String.fromFloat <| stationY - plot.range * cos plot.bearing * mapScale -- y is +ve downwards!
-                , r "5"
-                , stroke "orange"
-                , strokeWidth "1"
-                , A.fill "navy"
-                ]
+                ([ cx <| String.fromFloat <| stationX + plot.range * sin plot.bearing * mapScale
+                 , cy <| String.fromFloat <| stationY - plot.range * cos plot.bearing * mapScale -- y is +ve downwards!
+                 ]
+                    ++ plotStyles plot.plotType
+                )
                 []
     in
-    if model.actualTraceVisibleOnMap then
-        List.concatMap raidTrack model.targets
-
-    else
-        []
-
-
-userPlots model =
-    -- Note that the plots are associated with the polar targets.
-    -- We shall use the same conversion from range and bearing to map coordinates as we
-    -- do for the calculator grid so this should be fair.
-    -- Remember station is notionally in central square.
-    -- Scalewise, our map is made of 100km squares and they occupy 'squareSize' on the screen.
-    let
-        ( stationX, stationY ) =
-            stationPos model
-
-        drawThePlot plot =
-            Svg.circle
-                [ cx <| String.fromFloat <| stationX + plot.range * sin plot.bearing * mapScale -- x is +ve rightwards.
-                , cy <| String.fromFloat <| stationY - plot.range * cos plot.bearing * mapScale -- y is +ve downwards!
-                , r "5"
-                , stroke "navy"
-                , strokeWidth "1"
-                , A.fill "yellow"
-                ]
-                []
-    in
-    List.map drawThePlot model.storedPlots
+    List.map plotArrow plots
 
 
 ourStation model =
     let
         ( stationX, stationY ) =
-            stationPos model
+            stationPos
     in
     if model.rangeCircleVisibleOnMap then
         [ Svg.circle
