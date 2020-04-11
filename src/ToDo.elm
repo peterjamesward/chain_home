599ad285e0a9f648c08f2f37b0921aaabd71c6e4
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-04-11 10:37:39 +0100

    TODO Changes.

--- src/ToDo.elm
+++ src/ToDo.elm
@@ -1,4 +1,8 @@
 
+--TODO: Spinny vector addition in shader may obviate sorting.
+
+--TODO: Stop range slider messaging, or ignore them.
+
 --TODO: Logic to advance between play levels. Including button highlighting. Badges ?!
 --(Should I replace the all-at-once-visible buttons with ... what?)
 
@@ -6,9 +10,9 @@
 
 --TODO: Some level of information about every component (?).
 
+--TODO: Try layout on larger iPad screen.
+
 --TODO: Test on Android tablet !! (Currently using some webkit-only CSS tags to control touch actions.)
 
 --TODO: Reset WebGL time occasionally as the noise visibly degrades, or fix otherwise.
-
---TODO: Fit it on 800x600 screen?
 
