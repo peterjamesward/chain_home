5516475ba7aaf7e5701ce79f7285ab5d94e0433e
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-04-13 11:30:25 +0100

    TODO changes.

--- src/ToDo.elm
+++ src/ToDo.elm
@@ -1,18 +1,18 @@
 
+--TODO: Local storage for tutorials and game level. And Reset button.
+
+--TODO: Try layout on larger iPad screen.
+
+--TODO: Logic to advance between play levels. Including button highlighting. Badges ?!
+--(Should I replace the all-at-once-visible buttons with ... what?)
+
 --TODO: If I leave it running why does the map not "fill up"?
 
 --TODO: Don't store the plot when G&R pressed but operator left it in height mode.
 
 --TODO: Package as a Mac app? (Node.js, Electron ?)
 
---TODO: Logic to advance between play levels. Including button highlighting. Badges ?!
---(Should I replace the all-at-once-visible buttons with ... what?)
-
---TODO: Local storage for tutorials and game level. And Reset button.
-
 --TODO: Some level of information about every component (?).
-
---TODO: Try layout on larger iPad screen.
 
 --TODO: Test on Android tablet !! (Currently using some webkit-only CSS tags to control touch actions.)
 
