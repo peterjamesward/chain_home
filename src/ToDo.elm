176335a58813dac285ea014f491b93633d9d8f62
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-06-12 12:13:52 +0100

    Removed old TODOs.

--- src/ToDo.elm
+++ src/ToDo.elm
@@ -1,10 +1,5 @@
 
---TODO: Reorganise Learn & Play page - thinking a Panel for each raid type with buttons for learn and practice.
---TODO: Maybe practice should be prompted at each stage.
---TODO: Make Learn & Play the landing page?
 
 --TODO: Local storage for tutorials and game level. And Reset button.
 
 --TODO: Disable unwanted touch actions on all devices.
-
---TODO: Voice-over for tutorials?
