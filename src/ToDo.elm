53d7160d3bae3ee8acf7098408134f349fc44e43
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-10-02 15:04:06 +0100

    Improve steady increase of complexity and remove out of range targets.

--- src/ToDo.elm
+++ src/ToDo.elm
@@ -1,5 +1,3 @@
 
 --TODO: Show raid size on map, maybe by size of circle, maybe small raid plaque?
 
---TODO: Gradually increase frequency and size of raids, until we hit our limit of 14.
--- (Can we raise this limit? How much?)
