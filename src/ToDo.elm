51accebc249a3a9d3eefb45e87a9d8a52dc178ae
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-10-03 14:42:12 +0100

    TODOs.

--- src/ToDo.elm
+++ src/ToDo.elm
@@ -1,7 +1,4 @@
 
 --TODO: Show raid size on map, maybe by size of circle, maybe small raid plaque?
 
---TODO: Stretched lines still poor on low res displays. Maybe bring back "gradient"
---so we can make the beam brightnmess fall off less quickly when abs(slope) is high.
---Try deriving Y for X-epsilon and X+epsilon, where epsilon = half an increment of resolution;
---Y can be the average, and the difference we use to vertically anti-alias the beam.
+--TODO: Overlay on about page to highlight the menu options.
