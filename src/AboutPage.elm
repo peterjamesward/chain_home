36e16bd9fb307e860370dcdb4d482193d9125d72
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-10-03 15:34:52 +0100

    About text change only.

--- src/AboutPage.elm
+++ src/AboutPage.elm
@@ -18,6 +18,7 @@ aboutPage =
             , Font.size 22
             , padding 80
             , width fill
+            , spacing 8
             ]
             [ html content ]
         ]
@@ -28,15 +29,15 @@ content =
     Markdown.toHtmlWith { defaultOptions | smartypants = True }
         [ class "content" ]
         """
-This application aims to give an impression of being an operator
+This application shows the challenges facing an operator
 at a Chain Home station during the Battle of Britain in 1940.
 
 **Mode selection** lets you discover how different types of raid
- would appear on the tube, and how the operator would find each raid's data
- to be passed on to Fighter Command. You can then test your skills with a series of incoming raids.
+ would appear on the tube, and how the operator finds each raid's data
+ to pass on to Fighter Command. Test your skills with a series of incoming raids.
 
 **Receiver** is a slightly simplified mock-up of the main operator's control panel.
-This is what you use to detect raids and enter readings into the electrical calculator.
+Use this to detect raids and enter readings into the electrical calculator.
 
 **Calculator** illustrates the output from the electrical calculator that derives
  the position and height, and collates information for Fighter Command.
@@ -52,7 +53,7 @@ and Chrome, Firefox and Microsoft Edge browsers on Windows 10.
 It does **not** work with Internet Explorer 10 on Windows 10.
 It also works on iPad with iOS 13.4 or higher;
 the iPad Mini will work but the screen is too small to use effectively.
-No other tablets have been tested; feedback would be welcome but there is NO SUPPORT.
+No other tablets have been tested; feedback would be welcome but there is **_no support_**.
 
  * Application code written by Peter Ward.
  * Historical research by Sharon Ward.
