3ceb61d98a798a5a095bd56641295c2b37f4afc4
peter <peterjamesward@me.com>
2025-10-14 14:51:03 +0100

    wip: adding phase.

--- src/CRT_WebGL.elm
+++ src/CRT_WebGL.elm
@@ -5,6 +5,7 @@ import Html exposing (Html)
 import Html.Attributes exposing (height, style, width)
 import Math.Vector2 exposing (Vec2)
 import Math.Vector3 exposing (Vec3, vec3)
+import Math.Vector4 exposing (Vec4, vec4)
 import Messages exposing (Msg)
 import Types exposing (Echo, Point)
 import WebGL exposing (Mesh, Shader, clearColor)
@@ -70,42 +71,43 @@ type alias Uniforms =
     { iResolution : Vec3
     , iTime : Float
     , numRaids : Int
-    , raid0 : Vec3
-    , raid1 : Vec3
-    , raid2 : Vec3
-    , raid3 : Vec3
-    , raid4 : Vec3
-    , raid5 : Vec3
-    , raid6 : Vec3
-    , raid7 : Vec3
-    , raid8 : Vec3
-    , raid9 : Vec3
-    , raid10 : Vec3
-    , raid11 : Vec3
-    , raid12 : Vec3
-    , raid13 : Vec3
-    , raid14 : Vec3
-    , raid15 : Vec3
+    , raid0 : Vec4
+    , raid1 : Vec4
+    , raid2 : Vec4
+    , raid3 : Vec4
+    , raid4 : Vec4
+    , raid5 : Vec4
+    , raid6 : Vec4
+    , raid7 : Vec4
+    , raid8 : Vec4
+    , raid9 : Vec4
+    , raid10 : Vec4
+    , raid11 : Vec4
+    , raid12 : Vec4
+    , raid13 : Vec4
+    , raid14 : Vec4
+    , raid15 : Vec4
     }
 
 
-normaliseEcho : Echo -> Vec3
+normaliseEcho : Echo -> Vec4
 normaliseEcho echo =
-    vec3
+    vec4
         -- distance in metres, map to [0,1]
         (echo.r / 160000)
         -- empirically seems to be [0, 10], map to [0,1]
-        (logBase 10 (1 + echo.amplitude) / 10.0)
+        (logBase 10 (1 + echo.amplitude) / 8.0)
         -- number of craft.
         (toFloat echo.strength)
+        echo.phase
 
 
-echoToVec : Int -> Array Vec3 -> Vec3
+echoToVec : Int -> Array Vec4 -> Vec4
 echoToVec i echoes =
     -- With new graphics, keep this in world space, let the GPU sort it out.
     Array.get i echoes
         |> Maybe.withDefault
-            (vec3 0.0 0.0 0.0)
+            (vec4 0.0 0.0 0.0 0.0)
 
 
 uniforms : Float -> List Echo -> Uniforms
@@ -171,22 +173,22 @@ fragmentShader =
         uniform vec3 iResolution;
         uniform float iTime;
         uniform int numRaids;
-        uniform vec3 raid0;
-        uniform vec3 raid1;
-        uniform vec3 raid2;
-        uniform vec3 raid3;
-        uniform vec3 raid4;
-        uniform vec3 raid5;
-        uniform vec3 raid6;
-        uniform vec3 raid7;
-        uniform vec3 raid8;
-        uniform vec3 raid9;
-        uniform vec3 raid10;
-        uniform vec3 raid11;
-        uniform vec3 raid12;
-        uniform vec3 raid13;
-        uniform vec3 raid14;
-        uniform vec3 raid15;
+        uniform vec4 raid0;
+        uniform vec4 raid1;
+        uniform vec4 raid2;
+        uniform vec4 raid3;
+        uniform vec4 raid4;
+        uniform vec4 raid5;
+        uniform vec4 raid6;
+        uniform vec4 raid7;
+        uniform vec4 raid8;
+        uniform vec4 raid9;
+        uniform vec4 raid10;
+        uniform vec4 raid11;
+        uniform vec4 raid12;
+        uniform vec4 raid13;
+        uniform vec4 raid14;
+        uniform vec4 raid15;
 
         vec3 COL2 = vec3(0.0,1.0,0.2);
 
@@ -194,7 +196,8 @@ fragmentShader =
             return fract(sin(dot(pos.xyz, vec3(70.9898, 78.233, 32.4355))) * 43758.5453123);
         }
 
-        float raidContribution(vec3 raid, vec2 xy) {
+        // Each raid has an amplitude (x) and a phase (y).
+        vec2 raidContribution(vec4 raid, vec2 xy) {
             // x and y in [0,1],
             // raid.x is range in [0,1].
             // raid.y is calculated amplitude nominally in [0,1] ??
@@ -208,7 +211,7 @@ fragmentShader =
             float xDistForCube = clamp(0.0, 1.0, xDist * 20.0);
             float smoothed = smoothstep(1.0, 0.0, xDistForCube);
 
-            return amp * smoothed ;
+            return vec2(amp * smoothed * cos(raid.w), amp * smoothed * sin(raid.w));
         }
 
         // NOTE: Important to separate the notions of:
@@ -217,7 +220,7 @@ fragmentShader =
 
         float deriveSignalFromFieldsAt(vec2 xy) {
             // Each raid has a TBD signature that contributes to deflection.
-            float deflection = 0.01;
+            vec2 deflection = vec2 (0.01, 0.0);
             deflection += raidContribution(raid0, xy);
             deflection += raidContribution(raid1, xy);
             deflection += raidContribution(raid2, xy);
@@ -234,17 +237,19 @@ fragmentShader =
             deflection += raidContribution(raid13, xy);
             deflection += raidContribution(raid14, xy);
             deflection += raidContribution(raid15, xy);
-            deflection = clamp(1.0 - deflection, 0.0, 0.95);
+
+            float amplitude = clamp(1.0 - deflection.x, 0.0, 0.95);
+
+            //TODO: raids with >1 target
 
             // Intensity decays rapidly away from derived deflection.
             // Increase factor to focus the beam
-            float beamDistance = clamp(0.0,1.0,abs(deflection - xy.y) * 80.0);
+            float beamDistance = clamp(0.0,1.0,abs(amplitude - xy.y) * 80.0);
             float intensity = smoothstep( 1.0, 0.0, beamDistance );
 
             // Add some simple random noise, strong near the beam.
-            //float d2 = random(vec2(xy * 5. + iTime));
             float d3 = random(vec3(xy, iTime));
-            float randomSpread = clamp(0.0,d3,pow(1.0 - abs(deflection - xy.y),16.0));
+            float randomSpread = clamp(0.0,d3,pow(1.0 - abs(amplitude - xy.y),32.0));
 
             return  intensity + randomSpread;
         }
